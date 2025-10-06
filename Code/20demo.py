baee5cceb4faa6e1996597957743b73664d44e72
MariaShchekanova <maria.shchekanova124@gmail.com>
2025-10-06 10:05:49 -0700

    final

---
+++ Code/20demo.py
@@ -0,0 +1,28 @@
+'''
+t = 1, 2  # this is a tuple
+print(t)
+print(type(t))
+'''
+
+'''
+def midpoint(x1, y1, x2, y2):
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+    return x, y
+
+print(midpoint(1, 2, 3, 4))
+m = midpoint(1, 2, 3, 4)
+mx, my = midpoint(1, 2, 3, 4)
+print(mx, my)
+
+'''
+
+
+i = int('42')
+x = float('0.61803')
+print (i)
+print (x)
+print(i * x)
+
+x = float('hello')
+print (x)
